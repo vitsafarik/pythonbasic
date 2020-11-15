3c737013c1ee2e92f18de0db6cb85e2ffad69711
Vit Safarik <vit@safarik.dev>
2020-11-15 16:52:24 +0100

    files

--- classes.py
+++ classes.py
@@ -3,45 +3,51 @@
 # Create class
 class User:
 
-  # Constructor
-  def __init__(self, name, email, age):
-    self.name = name
-    self.email = email
-    self.age = age
-    
-    # Adding Encapsulation of variables... Encapsulation is the concept of making the variables non-accessible or accessible upto some extent from the child classes
-    self._private = 1000 # Encapsulated variables are declares with '_' in the constructor.
+    # Constructor
+    def __init__(self, name, email, age):
+        self.name = name
+        self.email = email
+        self.age = age
 
-  def greeting(self):
-      return f'My name is {self.name} and I am {self.age}'
+        # Adding Encapsulation of variables... Encapsulation is the concept of making the variables non-accessible or accessible upto some extent from the child classes
+        self._private = (
+            1000  # Encapsulated variables are declares with '_' in the constructor.
+        )
 
-  def has_birthday(self):
-      self.age += 1
- 
- #function for encap variable
-  def print_encap(self):
-      print(self._private)
+    def greeting(self):
+        return f"My name is {self.name} and I am {self.age}"
+
+    def has_birthday(self):
+        self.age += 1
+
+    # function for encap variable
+    def print_encap(self):
+        print(self._private)
+
 
 # Extend class
 class Customer(User):
-  # Constructor
-  def __init__(self, name, email, age):
-      User.__init__(self, name, email, age) #Called proper parent class constructor to make this as proper child inehriting all methods.
-      self.name = name
-      self.email = email
-      self.age = age
-      self.balance = 0
+    # Constructor
+    def __init__(self, name, email, age):
+        User.__init__(
+            self, name, email, age
+        )  # Called proper parent class constructor to make this as proper child inehriting all methods.
+        self.name = name
+        self.email = email
+        self.age = age
+        self.balance = 0
 
-  def set_balance(self, balance):
-      self.balance = balance
+    def set_balance(self, balance):
+        self.balance = balance
 
-  def greeting(self):
-      return f'My name is {self.name} and I am {self.age} and my balance is {self.balance}'
+    def greeting(self):
+        return f"My name is {self.name} and I am {self.age} and my balance is {self.balance}"
+
 
 #  Init user object
-brad = User('Brad Traversy', 'brad@gmail.com', 37)
+brad = User("Brad Traversy", "brad@gmail.com", 37)
 # Init customer object
-janet = Customer('Janet Johnson', 'janet@yahoo.com', 25)
+janet = Customer("Janet Johnson", "janet@yahoo.com", 25)
 
 janet.set_balance(500)
 print(janet.greeting())
@@ -49,15 +55,15 @@ print(janet.greeting())
 brad.has_birthday()
 print(brad.greeting())
 
-#Encapsulation -->
+# Encapsulation -->
 brad.print_encap()
-brad._private = 800 #Changing for brad
+brad._private = 800  # Changing for brad
 brad.print_encap()
 
 # Method inherited from parent
-janet.print_encap() #Changing the variable for brad doesn't affect janets variable --> Encapsulation
+janet.print_encap()  # Changing the variable for brad doesn't affect janets variable --> Encapsulation
 janet._private = 600
 janet.print_encap()
 
-#Similary changing janet's doesn't affect brad's variable.
+# Similary changing janet's doesn't affect brad's variable.
 brad.print_encap()
